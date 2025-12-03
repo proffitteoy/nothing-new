2801866a399602d8d095dbc5f9420669bf3807ad
﻿proffitteoy <204656371+proffitteoy@users.noreply.github.com>
2025-12-03 22:23:21 +0800

    ssh

--- 1.bat
+++ 1.bat
@@ -3,10 +3,12 @@ chcp 65001 >nul
 setlocal
 
 cd /d "%~dp0"
-set /p commit_msg=">"
+
+set /p commit_msg="Commit Message: "
 
 git add .
 git commit -m "%commit_msg%"
 git push origin main
 
 endlocal
+pause
